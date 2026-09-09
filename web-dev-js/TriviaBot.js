0fbcd40e9c60cc7f569e7c5e9f1294907236e721
krish2026c <krish2025f@gmail.com>
2026-09-09 18:23:20 +0530

    add trivia bot javascript program

---
+++ web-dev-js/TriviaBot.js
@@ -0,0 +1,19 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Bot Singh";
+let botLocation = "Bot pur";
+let favoriteLanguage = "C++";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = "Did you know? " + favoriteLanguage + " is widely used for system programming.";
+console.log(codingFact);
+
+codingFact = favoriteLanguage + " supports both procedural and object-oriented programming.";
+console.log(codingFact);
+
+codingFact = "Many popular software like games and operating systems are built using " + favoriteLanguage + ".";
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
